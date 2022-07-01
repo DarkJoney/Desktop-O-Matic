b25a10b6aa807ea1ce09bb7dc24cc089df920d51
DarkJoney <lest173@gmail.com>
2022-07-01 20:27:38 +0200

    timeline

--- engine/analysis.py
+++ engine/analysis.py
@@ -1,4 +1,4 @@
-import os, shutil
+import os, shutil, time
 
 
 def locate_desktop():
@@ -25,4 +25,27 @@ def move_to_folder(targets, fFormat):
         print("it exists!")
     for i in targets:
         print(i)
+        #to do copy here
+
+def move_to_folder_timeline(targets, fFormat):
+    targetFormat =  (str(fFormat).replace(".","")).upper()
+    path = locate_desktop() + "\\" + targetFormat
+    isExist = os.path.exists(path)
+    if not isExist:
+        os.makedirs(path)
+        print("The new directory is created!")
+    else:
+        print("it exists!")
+    for i in targets:
+        print(i)
+        f_mod_time = os.path.getmtime(i)
+        time_stamp = time.strftime("%Y-%m-%d", time.strptime(time.ctime(f_mod_time)))
+        print(time_stamp)
+        isExist = os.path.exists(path  + "\\" + time_stamp)
+        
+        if not isExist: 
+            os.makedirs(path  + "\\" + time_stamp)
+            print("The new directory is created!")
+        else:
+            print("it exists!")
         #to do copy here
--- main.py
+++ main.py
@@ -4,7 +4,8 @@ def main():
     print("hello!")
     desktopLoc = analysis.locate_desktop()
     locatedFiles = analysis.get_files(".pdf", desktopLoc)
-    analysis.move_to_folder(locatedFiles, ".pdf")
+    #analysis.move_to_folder(locatedFiles, ".pdf")
+    analysis.move_to_folder_timeline(locatedFiles, ".pdf")
 
 if __name__ == '__main__':
     main()
